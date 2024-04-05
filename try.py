8c4dd46bab9dddf0fdcb6175a611d91f6d9297f8
hiddenbebb <cbj4133@naver.com>
2024-04-05 17:05:56 +0900

    컴퓨터 확인 완료

--- try.py
+++ try.py
@@ -1,3 +1,7 @@
-from pykiwoom.kiwoom import *
+import pykiwoom
 
-kiwoom = Kiwoom()
+if __name__ == "__main__":
+    km = pykiwoom.KiwoomManager()
+    km.put_method(("GetMasterCodeName", "005930")) 
+    data = km.get_method()
+    print(data)
